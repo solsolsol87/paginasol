d2ab21421a22073941d394176b14dc0412bea108
sol bellusci <solbellusci@gmail.com>
2024-11-25 15:41:45 -0300

    Add files via upload

--- performance.js
+++ performance.js
@@ -6,6 +6,16 @@ let linkcasa= document.querySelector ("#linkcasa")
 let linkflojus= document.querySelector ("#linkflojus")
 let linklogica= document.querySelector ("#linklogica")
 
+let descripcionestado= document.querySelector ("#descripcion-estado")
+let descripcioncuerpo= document.querySelector ("#descripcion-cuerpo")
+let descripcioncasa= document.querySelector ("#descripcion-casa")
+let descripcionflojus= document.querySelector ("#descripcion-flojus")
+let descripcionlogica= document.querySelector ("#descripcion-logica")
+
+let descripciones = document.querySelectorAll (".descripcion article")
+
+
+//estadodeconexion
 linkestado.addEventListener ("mouseenter", 
     function ( ){
      
@@ -20,7 +30,21 @@ linkestado.addEventListener ("mouseleave",
     }
 )
 
+linkestado.addEventListener ("click",
+    function( ) {
 
+    for (let i=0; i<descripciones.length; i++) { // hace desaparece todas las descripciones  
+        descripciones[i] .classList.remove ("aparece")
+        descripciones[i] .classList.add ("oculto")
+
+    }
+    descripcionestado.classList.remove ("oculto")  // aca es cada una, la hacemos aparecer 
+    descripcionestado.classList.add ("aparece") 
+
+    }
+)
+
+//cuerpo transparente
 linkcuerpo.addEventListener ("mouseenter", 
     function ( ){
      
@@ -35,7 +59,22 @@ linkcuerpo.addEventListener ("mouseleave",
     }
 )
 
+linkcuerpo.addEventListener ("click",
+    function( ) {
 
+    for (let i=0; i<descripciones.length; i++) { // hace desaparece todas las descripciones  
+        descripciones[i] .classList.remove ("aparece")
+        descripciones[i] .classList.add ("oculto")
+
+    }
+    descripcioncuerpo.classList.remove ("oculto")  // aca es cada una, la hacemos aparecer 
+    descripcioncuerpo.classList.add ("aparece") 
+
+    }
+)
+
+
+//casatransparente
 linkcasa.addEventListener ("mouseenter", 
     function ( ){
      
@@ -50,7 +89,21 @@ linkcasa.addEventListener ("mouseleave",
     }
 )
 
+linkcasa.addEventListener ("click",
+    function( ) {
 
+    for (let i=0; i<descripciones.length; i++) { // hace desaparece todas las descripciones  
+        descripciones[i] .classList.remove ("aparece")
+        descripciones[i] .classList.add ("oculto")
+
+    }
+    descripcioncasa.classList.remove ("oculto")  // aca es cada una, la hacemos aparecer 
+    descripcioncasa.classList.add ("aparece") 
+
+    }
+)
+
+//flojus
 linkflojus.addEventListener ("mouseenter", 
     function ( ){
      
@@ -65,6 +118,22 @@ linkflojus.addEventListener ("mouseleave",
     }
 )
 
+
+linkflojus.addEventListener ("click",
+    function( ) {
+
+    for (let i=0; i<descripciones.length; i++) { // hace desaparece todas las descripciones  
+        descripciones[i] .classList.remove ("aparece")
+        descripciones[i] .classList.add ("oculto")
+
+    }
+    descripcionflojus.classList.remove ("oculto")  // aca es cada una, la hacemos aparecer 
+    descripcionflojus.classList.add ("aparece") 
+
+    }
+)
+
+// logicanalogica
 linklogica.addEventListener ("mouseenter", 
     function ( ){
      
@@ -79,6 +148,19 @@ linklogica.addEventListener ("mouseleave",
     }
 )
 
+linklogica.addEventListener ("click",
+    function( ) {
+
+    for (let i=0; i<descripciones.length; i++) { // hace desaparece todas las descripciones  
+        descripciones[i] .classList.remove ("aparece")
+        descripciones[i] .classList.add ("oculto")
+
+    }
+    descripcionlogica.classList.remove ("oculto")  // aca es cada una, la hacemos aparecer 
+    descripcionlogica.classList.add ("aparece") 
+
+    }
+)
 
 
 
